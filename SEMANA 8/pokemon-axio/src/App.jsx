956470fd23ad1362ea0f75b0337baf0a7bb926a4
Marileeh7 <marileeh7@gmail.com>
2024-05-21 11:24:03 -0400

    Semana 8

--- SEMANA 8/pokemon-axio/src/App.jsx
+++ SEMANA 8/pokemon-axio/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Pokemon from './components/Pokemon';
+import DetallesPokemon from './components/DetallesPokemon';
 import { useState } from 'react';
 import pokeballIcon from './assets/pngwing.com.png'; // Importar la imagen local
 
 function App() {
   const [nombresPokemon, setNombresPokemon] = useState([]);
+  const [selectedPokemonUrl, setSelectedPokemonUrl] = useState(null);
   const [error, setError] = useState(null);
 
   return (
@@ -14,13 +16,23 @@ function App() {
       {error && <p>{error}</p>}
       <ul id="pokemons">
         {nombresPokemon.map((pokemon, index) => (
-          <li key={index} className="card">
+          <li
+            key={index}
+            className="card"
+            onClick={() => setSelectedPokemonUrl(pokemon.url)}
+          >
             <img src={pokemon.image} alt={pokemon.name} className="pokemon-image" />
             <span className="card_name">{pokemon.name}</span>
             <img src={pokeballIcon} alt="Pokeball Icon" className="pokeball-icon" />
           </li>
         ))}
       </ul>
+      {selectedPokemonUrl && (
+        <div className="detalles-container">
+          <DetallesPokemon urlPokemon={selectedPokemonUrl} />
+          <button onClick={() => setSelectedPokemonUrl(null)} className="boton-cerrar">Cerrar</button>
+        </div>
+      )}
     </div>
   );
 }
