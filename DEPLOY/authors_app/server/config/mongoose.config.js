4f15c65134256faae6355dbb6895a35d58fe65c1
Marileeh7 <marileeh7@gmail.com>
2024-06-10 22:08:49 -0400

    Deploy2

--- DEPLOY/authors_app/server/config/mongoose.config.js
+++ DEPLOY/authors_app/server/config/mongoose.config.js
@@ -2,9 +2,6 @@ require ('dotenv').config();
 const mongoose = require("mongoose");
 
 const URI= process.env.MONGO_URI;
-mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(URI)
     .then(() => console.log("Conexión a la base de datos establecida"))
     .catch(err => console.log("ERROR:", err));
