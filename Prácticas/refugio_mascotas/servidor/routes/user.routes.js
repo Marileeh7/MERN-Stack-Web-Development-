81244a69be8dd07f87005dc8b488e67c70cba386
Marileeh7 <marileeh7@gmail.com>
2024-06-18 13:01:51 -0400

    practica

--- Prácticas/refugio_mascotas/servidor/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const UserController = require('../controllers/user.controllers');
-
-router.get('/user', UserController.getUser);
-
-module.exports = router;
