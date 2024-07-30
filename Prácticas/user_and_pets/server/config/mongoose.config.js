2645d864817d724470e396eb725a6aeae8dd0506
Maria Leticia Rodas <121524347+Marileeh7@users.noreply.github.com>
2024-07-30 08:54:44 -0400

    Delete Prácticas/user_and_pets directory

--- Prácticas/user_and_pets/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/petsDB', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Could not connect to MongoDB', err));
