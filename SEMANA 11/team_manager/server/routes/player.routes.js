357094d79ea4beea03df6ce81859477d1724ac18
Maria Leticia Rodas <121524347+Marileeh7@users.noreply.github.com>
2024-08-08 11:40:46 -0400

    Delete SEMANA 11 directory

--- SEMANA 11/team_manager/server/routes/player.routes.js
+++
@@ -1,7 +0,0 @@
-const PlayerController = require('../controllers/player.controller');
-
-module.exports = (app) => {
-  app.get('/players', PlayerController.getAllPlayers);
-  app.post('/players', PlayerController.addPlayer);
-  app.delete('/players/:id', PlayerController.deletePlayer);
-};
