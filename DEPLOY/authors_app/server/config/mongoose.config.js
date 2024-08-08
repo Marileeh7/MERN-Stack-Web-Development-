8bd7a1e7dd3995af0c2ada73c224d1756444ffd8
Maria Leticia Rodas <121524347+Marileeh7@users.noreply.github.com>
2024-08-08 11:39:28 -0400

    Delete DEPLOY directory

--- DEPLOY/authors_app/server/config/mongoose.config.js
+++
@@ -1,7 +0,0 @@
-require ('dotenv').config();
-const mongoose = require("mongoose");
-
-const URI= process.env.MONGO_URI;
-mongoose.connect(URI)
-    .then(() => console.log("Conexión a la base de datos establecida"))
-    .catch(err => console.log("ERROR:", err));
